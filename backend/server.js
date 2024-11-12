095e54b0047b1d60504f7bf6e1f742777e06868c
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-11-12 23:58:08 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/plans", planRoutes);
 app.use("/api/calorie",calorieTrackerRoutes);
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
