eee8ebf298050309bb17ebc5c16d7e60cf8bb0bf
Udayraj <110400788+UdayrajJhala@users.noreply.github.com>
2024-11-13 09:44:54 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 require("dotenv").config();
 const connectDB = require("./config/db");
 
@@ -15,8 +16,29 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/plans", planRoutes);
-app.use("/api/calorie",calorieTrackerRoutes);
+app.use("/api/calorie", calorieTrackerRoutes);
+
+app.get("/health", (req, res) => {
+  res.status(200).send("Server is running");
+});
+
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const pingServer = async () => {
+  try {
+    const response = await axios.get(
+      `https://fitmate-hp51.onrender.com/health`
+    );
+    if (response.status === 200) {
+      console.log("Server pinged successfully");
+    }
+  } catch (error) {
+    console.error("Error pinging server:", error.message);
+  }
+};
+
+setInterval(pingServer, 300000);
